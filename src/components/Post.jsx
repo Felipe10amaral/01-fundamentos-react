beefba14f211cc8b639a04c4169d187411d22d27
Felipe Amaral <felipeamaraldev15@gmail.com>
2024-11-21 10:27:13 -0300

    componente post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,24 @@
+import styles from './Post.module.css';
 
-export function Post(props) {
-    return (
-        <div>
-           
-            <h1>{props.author}</h1>
-            <p>{props.content}</p>
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/diego3g.png" />
+          <div className={styles.authorInfo}>
+            <strong>Felipe Amaral</strong>
+            <span>Web Developer</span>
+          </div>
         </div>
-    )
+        <time title="11 de Maio às 08:13h" dateTime="2024-11-11 08:13:00">Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉<a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
 }
